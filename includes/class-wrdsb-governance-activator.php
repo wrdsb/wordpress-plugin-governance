09c1a31faa3db634a46731f788072341918115b6
James Schumann <jschumann@gmail.com>
2017-02-10 07:42:17 -0500

    add boilerplate includes

---
+++ includes/class-wrdsb-governance-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.wrdsb.ca
+ * @since      1.0.0
+ *
+ * @package    WRDSB\Governance
+ * @subpackage WRDSB\Governance\includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    WRDSB\Governance
+ * @subpackage WRDSB\Governance\includes
+ * @author     WRDSB <website@wrdsb.on.ca>
+ */
+class WRDSB_Governance_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wrdsb-governance-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.wrdsb.ca
+ * @since      1.0.0
+ *
+ * @package    WRDSB\Governance
+ * @subpackage WRDSB\Governance\includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    WRDSB\Governance
+ * @subpackage WRDSB\Governance/includes
+ * @author     WRDSB <website@wrdsb.on.ca>
+ */
+class WRDSB_Governance_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
