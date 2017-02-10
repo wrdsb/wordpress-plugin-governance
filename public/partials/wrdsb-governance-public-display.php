d32400483e8120308d6b2625a473b1ed440607d3
James Schumann <jschumann@gmail.com>
2017-02-10 07:36:06 -0500

    add boilerplate public

---
+++ public/partials/wrdsb-governance-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://www.wrdsb.ca
+ * @since      1.0.0
+ *
+ * @package    WRDSB\Governance
+ * @subpackage WRDSB\Governance\public\partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
