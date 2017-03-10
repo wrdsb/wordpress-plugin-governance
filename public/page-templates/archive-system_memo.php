0291b056f69df03ea86cab56d6012fafda158b24
James Schumann <jschumann@gmail.com>
2017-03-10 07:52:34 -0500

    tinkering with system memo archive view

--- public/page-templates/archive-system_memo.php
+++ public/page-templates/archive-system_memo.php
@@ -2,90 +2,26 @@
 
 <div class="container">
   <div class="row">
+      <div class="col-sm-8 col-lg-8">
 
-    <?php $has_left = FALSE; ?>
-    <?php $has_right = FALSE; ?>
-    <?php if (is_active_sidebar('sidebar-left') || has_nav_menu('left')) {$has_left = TRUE;} ?>
-    <?php if (is_active_sidebar('sidebar-right') || has_nav_menu('right')) {$has_right = TRUE;} ?>
+        <?php
+        // Start the Loop.
+        while ( have_posts() ) : the_post();
+          the_title( '<h2 class="news"><a href="'. esc_url( get_permalink() ) .'" rel="bookmark">'. strtoupper($post->post_name) .': ', '</a></h2>' );      
+          echo 'Posted '. get_the_date();
+          if ( has_excerpt() ) {
+            the_excerpt();
+            echo '<p class="readmore"><a href="'. get_permalink($post->ID) . '"><strong>Read more about</strong> <cite>'. get_the_title($post->ID) .'</cite> &#187;</a></p>';
+          } else {
+            the_excerpt();
+          }
+        endwhile;
+        ?>
 
-    <?php
-    # Both sidebars
-    if (($has_left == TRUE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-3 col-md-2 col-lg-2">';
-      get_sidebar('left');
-      echo '</div>';
+      </div> <!-- end content area -->
 
-    # Just left sidebar
-    elseif (($has_left == TRUE) and ($has_right == FALSE)):
-      echo '<div class="col-sm-3 col-lg-2">';
-      get_sidebar('left');
-      echo '</div>';
-
-    # Just right sidebar
-      # Nothing to do
-
-    # No sidebars
-      # Nothing to do
-    endif
-    ?>
-
-    <?php
-    # Both sidebars
-    if (($has_left == TRUE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-6 col-md-8 col-lg-8">';
-
-    # Just left sidebar
-    elseif (($has_left == TRUE) and ($has_right == FALSE)):
-      echo '<div class="col-sm-9 col-lg-10">';
-
-    # Just right sidebar
-    elseif (($has_left == FALSE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-9">';
-
-    # No sidebars
-    elseif (($has_left == FALSE) and ($has_right == FALSE)):
-      echo '<div class="col-sm-12 col-lg-12">';
-
-    endif
-    ?>
-
-    <?php
-      // Start the Loop.
-      while ( have_posts() ) : the_post();
-      
-        // Include the post format-specific content template.
-        get_template_part( 'content', get_post_format() );
-
-        // If comments are open or we have at least one comment, load up the comment template.
-        if ( comments_open() || get_comments_number() ) {
-          comments_template();
-        }
-      endwhile;
-    ?>
-
-    </div> <!-- end content area -->
-
-    <?php
-    # Both sidebars
-    if (($has_left == TRUE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-3 col-md-2 col-lg-2">';
-      get_sidebar('right');
-      echo '</div>';
-
-    # Just left sidebar
-      # Nothing to do
-
-    # Just right sidebar
-    elseif (($has_left == FALSE) and ($has_right == TRUE)):
-      echo '<div class="col-sm-3">';
-      get_sidebar('right');
-      echo '</div>';
-
-    # No sidebars
-      # Nothing to do
-
-    endif
-    ?>
+      <div class="col-sm-4 col-lg-8">
+      </div>
 
   </div>
 </div>
