77049109dc9572589af81b381a131bccd8e5aa8f
James Schumann <jschumann@gmail.com>
2017-03-10 10:43:09 -0500

    tweaking system memo single and archive templates

--- public/page-templates/archive-system_memo.php
+++ public/page-templates/archive-system_memo.php
@@ -8,7 +8,7 @@
         // Start the Loop.
         while ( have_posts() ) : the_post();
           the_title( '<h2 class="news"><a href="'. esc_url( get_permalink() ) .'" rel="bookmark">'. strtoupper($post->post_name) .': ', '</a></h2>' );      
-          echo 'Posted '. get_the_date();
+          echo '<small class="gray-dark">Posted: '. get_the_date('F j, Y') .' at '. get_the_time('g:i a') .'</small>';
           if ( has_excerpt() ) {
             the_excerpt();
             echo '<p class="readmore"><a href="'. get_permalink($post->ID) . '"><strong>Read more about</strong> <cite>'. get_the_title($post->ID) .'</cite> &#187;</a></p>';
